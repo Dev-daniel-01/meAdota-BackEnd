c06c6c63d3d477207f147e7b5d4454b9c72865c1
Pichau <Ribeirodacostadaniel57@gmail.com>
2025-07-09 14:55:41 -0300

    Postmans

---
+++ prisma/migrations/20250709165140_ajuste_delete_pet/migration.sql
@@ -0,0 +1,19 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Feedback" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "comment" TEXT NOT NULL,
+    "rating" INTEGER NOT NULL,
+    "image" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" INTEGER NOT NULL,
+    "petId" INTEGER NOT NULL,
+    CONSTRAINT "Feedback_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Feedback_petId_fkey" FOREIGN KEY ("petId") REFERENCES "Pet" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_Feedback" ("comment", "createdAt", "id", "image", "petId", "rating", "userId") SELECT "comment", "createdAt", "id", "image", "petId", "rating", "userId" FROM "Feedback";
+DROP TABLE "Feedback";
+ALTER TABLE "new_Feedback" RENAME TO "Feedback";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
